ee87b7895d53803dc22f9e46267d4c62f8064bf3
Abdallah Ali <amaali1991@gmail.com>
2025-02-08 17:36:00 +0200

    add sddm theme

--- modules/nixos/desktop/plasma/default.nix
+++ modules/nixos/desktop/plasma/default.nix
@@ -9,5 +9,7 @@ in {
 
   config = mkIf cfg.enable {
     services.desktopManager.plasma6.enable = true;
+    services.xserver.displayManager.sddm.theme = "sugar-dark";
+    environment.systemPackages = with pkgs.afxq; [ sugar-dark ];
   };
 }
